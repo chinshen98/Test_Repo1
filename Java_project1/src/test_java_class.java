5a71cb3e8dae52188be403ea22f5c46ef3eb13cb
Chin Shen <chinshen.lee@octafaconsulting.com>
2022-03-18 16:11:42 +0800

    Adding 2.1 ver message

--- Java_project1/src/test_java_class.java
+++ Java_project1/src/test_java_class.java
@@ -5,5 +5,6 @@ public class test_java_class {
         System.out.println("Test working on the GitHub and TortoiseGit.");
         System.out.println("Printing the text now.");
         System.out.println("Adding new sentences into this java class file.");
+        System.out.println("Adding 2.0 new sentences into this java class file");
     }
 }
