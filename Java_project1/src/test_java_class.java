1daec58358a96a9eb9dc806392ce5f9838e6bcff
Chin Shen <chinshen.lee@octafaconsulting.com>
2022-03-18 11:36:48 +0800

    Added new project file to the github

---
+++ Java_project1/src/test_java_class.java
@@ -0,0 +1,9 @@
+public class test_java_class {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test working on the GitHub and TortoiseGit.");
+        System.out.println("Printing the text now.");
+        System.out.println("Here.");
+    }
+}
