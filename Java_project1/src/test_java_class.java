7a4a51c2ecd1753bc11057273f1bb9cc293c05ef
chinshen98 <101780306+chinshen98@users.noreply.github.com>
2022-03-18 16:32:58 +0800

    Update test_java_class.java

--- Java_project1/src/test_java_class.java
+++ Java_project1/src/test_java_class.java
@@ -6,5 +6,7 @@ public class test_java_class {
         System.out.println("Printing the text now.");
         System.out.println("Adding new sentences into this java class file.");
         System.out.println("Adding 2.1 new sentences into this java class file");
+        
+        ##Test Adding text from GitHub and pull to local using TortoiseGIt.
     }
 }
